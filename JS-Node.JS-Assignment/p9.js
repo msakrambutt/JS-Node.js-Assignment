95c944cf4763e48a08eb82c71031d73691e55d25
Shahid Akram Butt <108168739+msakrambutt@users.noreply.github.com>
2022-10-15 19:43:44 +0500

    Update p9.js

--- JS-Node.JS-Assignment/p9.js
+++ JS-Node.JS-Assignment/p9.js
@@ -4,22 +4,24 @@ console.log("Output Excercise # 10\n");
 
 /* Program # This program we save student’s bio data in JS variables and show the
 data on console*/
-var student_name = "shahid akram butt";
-var student_age=40;
-var student_course="Certified Metaverse Web 3.0";
-console.log(student_name+"\n");
-console.log(student_age+" years old\n");
-console.log(student_course+"\n");
-
+//Start of first program
+var student_name = "shahid akram butt";// Declare & intialize JS Variable of Student Name
+var student_age=40;//Declare & intialize JS Variable of Student age
+var student_course="Certified Metaverse Web 3.0";// Declare & intialize JS Variable of Student Course
+console.log(student_name+"\n");// Output Student name on Console
+console.log(student_age+" years old\n");//Output Student age on Console
+console.log(student_course+"\n");//Output Student Course on Console
+//End of first program
 /* Assume we have 10 US dollars & 25 Saudi Riyals. Write a script to
 convert the total currency to Pakistani Rupees
 */
+//Start of Second program
+var us_Dollar=235;//Declare & intialize JS Variable of us_Dollar
+var saudi_Riyal=51;//Declare & intialize JS Variable of saudi_Riyal
 
-var us_Dollar=235;
-var saudi_Riyal=51;
-
-console.log("Currency in PKR\n");
-console.log("Total Currency in PKR: "+(us_Dollar*10+saudi_Riyal*25)+"\n");
+console.log("Currency in PKR\n");//Output on console
+console.log("Total Currency in PKR: "+(us_Dollar*10+saudi_Riyal*25)+"\n");// Ouput Result on console
+//End of Second program
 
 
 
